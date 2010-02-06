1964865636d933029be5c20decd6073a08c21a49
TekWiz <warlickt@operissystems.com>
2010-02-06 13:40:22 -0600

    extract options for has_attached_file into module

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,8 @@
 require 'mime_type_ext'
  
 class Asset < ActiveRecord::Base
+  extend AttachmentOptions
+  
   @@known_types = []
   cattr_accessor :known_types
  
@@ -168,18 +170,7 @@ class Asset < ActiveRecord::Base
   
   # order_by 'title'
  
-  has_attached_file :asset,
-                    :processors => lambda {|instance| instance.choose_processors },   # this allows us to set processors per file type, and to add more in other extensions
-                    :styles => lambda { thumbnail_definitions },                      # and this lets extensions add thumbnailers (and also usefully defers the call)
-                    :whiny_thumbnails => false,
-                    :storage => Radiant::Config["assets.storage"] == "s3" ? :s3 : :filesystem, 
-                    :s3_credentials => {
-                      :access_key_id => Radiant::Config["assets.s3.key"],
-                      :secret_access_key => Radiant::Config["assets.s3.secret"]
-                    },
-                    :bucket => Radiant::Config["assets.s3.bucket"],
-                    :url => Radiant::Config["assets.url"] ? Radiant::Config["assets.url"] : "/:class/:id/:basename:no_original_style.:extension", 
-                    :path => Radiant::Config["assets.path"] ? Radiant::Config["assets.path"] : ":rails_root/public/:class/:id/:basename:no_original_style.:extension"
+  has_attached_file :asset, attachment_options
                                  
   has_many :page_attachments, :dependent => :destroy
   has_many :pages, :through => :page_attachments
@@ -268,5 +259,95 @@ class Asset < ActiveRecord::Base
     def assign_title
       self.title = basename if title.blank?
     end
-    
+  
+  # = AttachmentOptions
+  #
+  # Encapsulates options for the +has_attached_file+ declaration in the Asset module
+  #
+  module AttachmentOptions
+  
+    ##
+    # Options for +has_attached_file+'.  Uses Radiant::Config['assets.storage'] to determine
+    # the appropriate set of options: +filesystem+ (default), +cloud_files+ or 'cloud_file', and 's3'
+    # 
+    # options: overrides for +has_attached_file+
+    #
+    def attachment_options( options = {} )
+      case _c('assets.storage')
+      when 's3'
+        s3_attachment_options
+      when 'cloud_files', 'cloud_file'
+        cloud_file_attachment_options
+      when nil, '', 'filesystem'
+        filesystem_attachment_options
+      else
+        raise 'Unkown assets storage engine: '+_c('assets.storage').to_s
+      end.merge(options)
+    end
+  
+    ##
+    # Builder for filesystem storage
+    #
+    def filesystem_attachment_options
+      attachment_options {
+        :storage => :filesystem
+      }
+    end
+  
+    ##
+    # Builder for AWS S3 storage
+    #
+    def s3_attachment_options
+      attachment_options {
+        :storage => :s3,
+        :bucket => _c('assets.s3.bucket'),
+        # :s3_host_alias => _c('assets.s3.host_alias'), # TODO add cloud front support
+        :s3_credentials => {
+          :access_key_id => _c('assets.s3.key'),
+          :secret_access_key => _c('assets.s3.secret'),
+        }
+      }
+    end
+  
+    ##
+    # Builder for Rackspace CloudFiles storage
+    #
+    def cloud_file_attachment_options
+      attachment_options {
+        :storage => :cloud_file,
+        :container => _c('assets.cloud_files.container'),
+        :cloudfiles_credentials => {
+          :username => _c('assets.cloud_files.username'),
+          :api_key => _c('assets.cloud_files.api_key'),
+          :servicenet => Boolean(_c('assets.cloud_files.servicenet'))
+        }
+      }
+    end
+
+  protected
+
+    ##
+    # The base for the builders.
+    #
+    # options: overrides for +has_attached_file+
+    #
+    def attachment_options(options = {})
+      {
+        # this allows us to set processors per file type, and to add more in other extensions
+        :processors => lambda {|instance| instance.choose_processors },
+        # and this lets extensions add thumbnailers (and also usefully defers the call)
+        :styles => lambda { thumbnail_definitions },
+        :whiny_thumbnails => false,
+        :url => _c('assets.url') ? _c('assets.url') : "/:class/:id/:basename:no_original_style.:extension", 
+        :path => _c('assets.path') ? _c('assets.path') : ":rails_root/public/:class/:id/:basename:no_original_style.:extension"
+      }.merge(options)
+    end
+  
+    alias :_c, :radiant_config
+    def radiant_config( key )
+      v = Radiant::Config[key] 
+      v = v.strip unless v.blank? or v.strip.blank?
+      return v
+    end
+  end
 end
